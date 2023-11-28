0ec04931e5c95c99dda402b84e56eb69f7868dde
cckim <r2222911r@ajou.ac.kr>
2023-11-28 16:09:56 +0900

    feat: Member, ParkingLot, parkingSpace, Role, Vehicle entity

---
+++ src/main/java/db/entity/Member.java
@@ -0,0 +1,46 @@
+package db.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String memberName;
+
+    @Column(nullable = false)
+    private String memberId;
+
+    @Column(nullable = false)
+    private String memberPassword;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private Date createAt;
+
+    @OneToMany(mappedBy = "member")
+    @Cascade(CascadeType.ALL)
+    private List<Vehicle> vehicle;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
---
+++ src/main/java/db/entity/ParkingLot.java
@@ -0,0 +1,38 @@
+package db.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParkingLot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String location;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String company;
+
+    @OneToMany(mappedBy = "parkingLot")
+    @Cascade(CascadeType.ALL)
+    private List<ParkingSpace> space;
+
+}
