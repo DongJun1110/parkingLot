f05b51ef885306dfbf893e4204d7fbed561ae003
cckim <r2222911r@ajou.ac.kr>
2023-12-02 21:53:03 +0900

    feat: index.html 추가

---
+++ parkinglot/src/main/java/db/parkinglot/ParkinglotApplication.java
@@ -0,0 +1,13 @@
+package db.parkinglot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinglotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkinglotApplication.class, args);
+	}
+
+}
---
+++ parkinglot/src/test/java/db/parkinglot/ParkinglotApplicationTests.java
@@ -0,0 +1,13 @@
+package db.parkinglot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinglotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
