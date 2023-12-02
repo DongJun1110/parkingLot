6df4b318a71228004fa27d9778ad9286649e149b
cckim <r2222911r@ajou.ac.kr>
2023-12-02 23:50:47 +0900

    feat: name과 location 기반 찾기 메서드 추가

--- src/main/java/db/parkinglot/repository/ParkingLotRepository.java
+++ src/main/java/db/parkinglot/repository/ParkingLotRepository.java
@@ -3,6 +3,8 @@ package db.parkinglot.repository;
 import db.parkinglot.entity.ParkingLot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ParkingLotRepository extends JpaRepository<ParkingLot, Long> {
+import java.util.List;
 
+public interface ParkingLotRepository extends JpaRepository<ParkingLot, Long> {
+    List<ParkingLot> findParkingLotByNameContainingLAndLocationContaining(String keyword);
 }
