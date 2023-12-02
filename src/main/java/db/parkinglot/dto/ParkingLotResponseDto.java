f09f654c8d6f290a650699b48b2935037106b353
cckim <r2222911r@ajou.ac.kr>
2023-12-02 21:53:33 +0900

    feat: parkinglot list dto 추가

---
+++ src/main/java/db/parkinglot/dto/ParkingLotResponseDto.java
@@ -0,0 +1,36 @@
+package db.parkinglot.dto;
+
+import db.parkinglot.entity.ParkingLot;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ParkingLotResponseDto {
+
+    private String name;
+    private String location;
+    private int totalSpace;
+    private int leftSpace;
+    private String company;
+    private String fee;
+    private String sort;
+    private String start_time;
+    private String end_time;
+
+    public static ParkingLotResponseDto toDto(ParkingLot parkingLot) {
+        return ParkingLotResponseDto.builder()
+                .name(parkingLot.getName())
+                .location(parkingLot.getLocation())
+                .totalSpace(parkingLot.getTotalSpace())
+                .leftSpace(parkingLot.getLeftSpace())
+                .company(parkingLot.getCompany())
+                .fee(parkingLot.getFee())
+                .sort(parkingLot.getSort())
+                .start_time(parkingLot.getStartTime())
+                .end_time(parkingLot.getEndTime())
+                .build();
+    }
+}
