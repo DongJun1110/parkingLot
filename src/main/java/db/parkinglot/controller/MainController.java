059bcc7fa465e4a509fd39cb7cf22bb3775c0643
cckim <r2222911r@ajou.ac.kr>
2023-12-05 23:21:07 +0900

    fix: 메인페이지 업데이트

--- src/main/java/db/parkinglot/controller/MainController.java
+++ src/main/java/db/parkinglot/controller/MainController.java
@@ -1,13 +1,23 @@
 package db.parkinglot.controller;
 
+import db.parkinglot.dto.UserInfoDto;
+import db.parkinglot.entity.Member;
+import db.parkinglot.repository.MemberRepository;
+import db.parkinglot.security.SecurityUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Optional;
+
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class MainController {
+
+    private final MemberRepository memberRepository;
 
     @GetMapping("/parkingList")
     @ResponseBody
@@ -15,15 +25,25 @@ public class MainController {
         return "parkingList";
     }
 
-    @ResponseBody
     @GetMapping("/parkingRegister")
     public String parkingRegister() {
-        return "parkingRegister";
+        String userId = SecurityUtil.getCurrentMemberId().getUserId();
+
+        Optional<Member> byUserId = memberRepository.findByUserId(userId);
+        log.info("유저 아이디" + userId);
+
+        return "register/register";
     }
 
     @ResponseBody
     @GetMapping("/myPage")
     public String my() {
+
+        UserInfoDto currentMemberId = SecurityUtil.getCurrentMemberId();
+        if(currentMemberId == null){
+            return "auth/login";
+        }
+
         return "마이페이지";
     }
 
