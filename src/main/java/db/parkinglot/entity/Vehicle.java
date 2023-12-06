db31208670dc328150749e8804b41ab39f5d319f
cckim <r2222911r@ajou.ac.kr>
2023-12-06 20:57:33 +0900

    chore: createAt 필드 생성

--- src/main/java/db/parkinglot/entity/Vehicle.java
+++ src/main/java/db/parkinglot/entity/Vehicle.java
@@ -1,33 +1,40 @@
 package db.parkinglot.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
+
+import java.util.Date;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Builder
 public class Vehicle {
 
-    @Id @GeneratedValue
-    private String carId;
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     @Column(nullable = false)
     private String brand;
 
+    @Column(nullable = false)
+    private String number;
+
     @ManyToOne(targetEntity = Member.class)
     @Cascade(CascadeType.ALL)
+    @JsonIgnore
     @JoinColumn(name = "memberId")
     private Member member;
 
     @Column(nullable = false)
     private String color;
+
+    private Date createdAt;
 
 }
