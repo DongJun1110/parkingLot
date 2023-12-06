f741242e0c9d360084eb8a4c8186e7d81bf5eea6
cckim <r2222911r@ajou.ac.kr>
2023-12-06 11:11:45 +0900

    feat: Vehicle class 추가

---
+++ src/main/java/db/parkinglot/entity/Vehicle.java
@@ -0,0 +1,33 @@
+package db.parkinglot.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Vehicle {
+
+    @Id @GeneratedValue
+    private String carId;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @ManyToOne(targetEntity = Member.class)
+    @Cascade(CascadeType.ALL)
+    @JoinColumn(name = "memberId")
+    private Member member;
+
+    @Column(nullable = false)
+    private String color;
+
+}
