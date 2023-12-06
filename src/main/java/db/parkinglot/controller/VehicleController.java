b971da3b4d6ddf9d41f784765cf46e3bdfc0ddf9
cckim <r2222911r@ajou.ac.kr>
2023-12-06 20:52:35 +0900

    feat: vehicle 컨트롤러 생성

---
+++ src/main/java/db/parkinglot/controller/VehicleController.java
@@ -0,0 +1,37 @@
+package db.parkinglot.controller;
+
+import db.parkinglot.dto.VehicleRequestDto;
+import db.parkinglot.entity.Vehicle;
+import db.parkinglot.service.VehicleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/vehicle")
+public class VehicleController {
+
+    private final VehicleService vehicleService;
+
+    @GetMapping("/registeredList")
+    @ResponseBody
+    public List<Vehicle> showVehicleList() {
+        return vehicleService.getVehicles();
+    }
+
+    @GetMapping("/register")
+    public String getRegisterForm() {
+        return "vehicle/register";
+    }
+
+    @PostMapping("/register")
+    @ResponseBody
+    public Vehicle registerVehicle(@RequestBody VehicleRequestDto vehicleRequestDto) {
+        Vehicle vehicle = vehicleService.registerVehicle(vehicleRequestDto);
+        return vehicle;
+    }
+
+}
