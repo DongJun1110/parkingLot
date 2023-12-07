d471452b554324a72949a73bfbc81f448e279ca0
cckim <r2222911r@ajou.ac.kr>
2023-12-07 11:56:39 +0900

    fix:뷰 이동 컨트롤러 삭제

--- src/main/java/db/parkinglot/controller/ParkingLotRegisterController.java
+++ src/main/java/db/parkinglot/controller/ParkingLotRegisterController.java
@@ -1,10 +1,8 @@
 package db.parkinglot.controller;
 
 import db.parkinglot.dto.ParkingLotRequestDto;
-import db.parkinglot.entity.ParkingLot;
 import db.parkinglot.service.ParkingLotService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,11 +21,6 @@ public class ParkingLotRegisterController {
     public ResponseEntity<String> saveParkingLot(@RequestBody ParkingLotRequestDto pld) {
         parkingLotService.registerParkingLot(pld);
         return ResponseEntity.ok("Parking Lot added successfully");
-    }
-
-    @GetMapping("/registerForm")
-    public String getRegisterForm() {
-        return "register/register";
     }
 
 }
