6a9acbb4f0e749c77e6ddf36b95edc6e2a8d12b7
cckim <r2222911r@ajou.ac.kr>
2023-12-07 11:58:42 +0900

    fix: area 키워드로 검색하는 메서드 추가

--- src/main/java/db/parkinglot/repository/ChauffeurRepository.java
+++ src/main/java/db/parkinglot/repository/ChauffeurRepository.java
@@ -1,8 +1,11 @@
 package db.parkinglot.repository;
 
 import db.parkinglot.entity.Chauffeur;
+import db.parkinglot.entity.ParkingLot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChauffeurRepository extends JpaRepository<Chauffeur, Long> {
+import java.util.List;
 
+public interface ChauffeurRepository extends JpaRepository<Chauffeur, Long> {
+    List<Chauffeur> findChauffeurByArea(String keyword);
 }
