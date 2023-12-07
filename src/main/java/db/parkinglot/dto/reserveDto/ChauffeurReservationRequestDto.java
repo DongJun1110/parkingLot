4723273344e0b6b1b740b23fce9f57e47475e717
cckim <r2222911r@ajou.ac.kr>
2023-12-07 14:09:44 +0900

    feat: 대리운전 예약 requestDto

--- src/main/java/db/parkinglot/dto/reserveDto/ChauffeurReservationRequestDto.java
+++ src/main/java/db/parkinglot/dto/reserveDto/ChauffeurReservationRequestDto.java
@@ -1,14 +1,12 @@
 package db.parkinglot.dto.reserveDto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class ChauffeurReservationRequestDto {
 
     private String carNumber;
